642cc4cd8dcb16f225af867cb56789ea7f4be506
Ripan Fauzi <ripanfauzi999@gmail.com>
2021-10-06 10:12:29 +0700

    include library jquery cookie and fix transaction 403 forbidden

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -4,6 +4,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
 <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
 <script>
     $(document).ready(function() {
         <?php foreach($this->pageInfo['nav_ids'] as $key):?>
